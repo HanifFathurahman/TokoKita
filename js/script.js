0035020088a38ed231c5a6081920ea7212288eca
Hanif Fathurahman <58112881+HanifFathurahman@users.noreply.github.com>
2024-12-09 19:02:53 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let cartCount = 0;
+let subtotal = 0;
 
 function addToCart(price) {
     cartCount++;
+    subtotal += price;
     document.getElementById('cart-count').innerText = cartCount;
-    alert(`Produk berhasil ditambahkan ke keranjang! Total barang: ${cartCount}`);
+    alert(`Produk berhasil ditambahkan ke keranjang!\nTotal barang: ${cartCount}\nTotal harga: Rp ${subtotal}`);
 }
